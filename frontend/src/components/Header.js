4ce21544ad6c1b06df59f87c0132b4ea0f023c17
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-17 22:53:53 +0530

    react router done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,43 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../assets/logo.png'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from "../assets/logo.png";
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect  style={{
-                    background: 'linear-gradient(to right, #3a7bd5, #6937a1)',
-                }}>
+            <Navbar
+                bg="dark"
+                variant="dark"
+                expand="lg"
+                collapseOnSelect
+                style={{
+                    background: "linear-gradient(to right, #3a7bd5, #6937a1)",
+                }}
+            >
                 <Container>
-                    <Navbar.Brand href='/'>
-                    <img src={logo} alt='shopify' />
-                    Shopify</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ms-auto'>
-                            <Nav.Link href='/cart'>
+                <LinkContainer to='/'>
+                    <Navbar.Brand >
+                        <img src={logo} alt="shopify" />
+                        Shopify
+                    </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                        <LinkContainer to='/cart'>
+                            <Nav.Link>
                                 <FaShoppingCart /> Cart
                             </Nav.Link>
-                            <Nav.Link href='/login'>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                            <Nav.Link>
                                 <FaUser /> Sign In
                             </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
+                    
                 </Container>
             </Navbar>
         </header>
