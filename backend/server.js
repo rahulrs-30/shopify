2b1a52883525362301466c5ffea6d2a2fedbe7ab
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-19 04:08:18 +0530

    product controller midway

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 const port=process.env.PORT;
 
@@ -14,8 +14,12 @@ const app = express();
 app.get('/', (req,res) => {
     res.send('API is running...');
 });
+//Product routes
 app.use('/api/products', productRoutes);
 
+//Error handling middleware 404 and other errors
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
