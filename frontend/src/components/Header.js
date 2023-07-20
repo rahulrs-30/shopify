0eae093633665346003147c92fdda194b3ee00c4
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-21 01:55:06 +0530

    cart section done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,56 +1,53 @@
 import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-    const { cartItems } = useSelector((state)=> state.cart);
-    return (
-        <header>
-            <Navbar
-                bg="dark"
-                variant="dark"
-                expand="lg"
-                collapseOnSelect
-                style={{
-                    background: "linear-gradient(to right, #3a7bd5, #6937a1)",
-                }}
-            >
-                <Container>
-                <LinkContainer to='/'>
-                    <Navbar.Brand >
-                        <img src={logo} alt="shopify" />
-                        Shopify
-                    </Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                        <LinkContainer to='/cart'>
-                            <Nav.Link>
-                                <FaShoppingCart /> Cart
-                                {
-                                    cartItems.length > 0 && (
-                                        <Badge pill bg='success' style={{marginLeft: '5px'}}>
-                                            {cartItems.reduce((a,c)=> a+c.qty,0)}
-                                        </Badge>
-                                    )
-                                }
-                            </Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/login'>
-                            <Nav.Link>
-                                <FaUser /> Sign In
-                            </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                    
-                </Container>
-            </Navbar>
-        </header>
-    );
+  const { cartItems } = useSelector((state) => state.cart);
+  return (
+    <header>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+        style={{
+          background: "linear-gradient(to right, #3a7bd5, #6937a1)",
+        }}
+      >
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="shopify" />
+              Shopify
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
