f086ceaac036614a5f513eac0b878ab1ba74482f
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-21 02:52:36 +0530

    basic authentication done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port=process.env.PORT;
 
 
@@ -11,11 +12,16 @@ connectDB(); //Connect to MongoDB
 
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
 app.get('/', (req,res) => {
     res.send('API is running...');
 });
 //Product routes
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 //Error handling middleware 404 and other errors
 app.use(notFound);
