890b600040be6cc5f6f1ad26bf3d9be7fceaf209
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-26 19:11:12 +0530

    paypal integration done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,7 +65,7 @@ const getOrderById = asyncHandler(async (req,res) => {
 const updateOrderToPaid = asyncHandler(async (req,res) => {
     const order = await Order.findById(req.params.id);
     if(order){
-        order.isPaid =true;
+        order.isPaid = true;
         order.paidAt = Date.now();
         order.paymentResult = {
             id: req.body.id,
