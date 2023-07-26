9ae3d6f40f6df0201f970a8ff5806e7c81e545d3
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-27 02:53:56 +0530

    list orders for admin

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -34,9 +34,15 @@ export const orderApiSlice = apiSlice.injectEndpoints({
                 url:`${ORDERS_URL}/mine`,
             }),
             keepUnusedDataFor: 5, 
+        }),
+        getOrders: builder.query({
+            query: ()=>({
+                url: ORDERS_URL,
+            }),
+            keepUnusedDataFor: 5,
         })
     }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery, useGetMyOrdersQuery } =
-    orderApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation,
+    useGetPayPalClientIdQuery, useGetMyOrdersQuery, useGetOrdersQuery } = orderApiSlice;
