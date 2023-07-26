fd11a58d4a3f35021187b6b5a463d618906ae955
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-26 20:26:26 +0530

    checkout process completed

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -28,9 +28,15 @@ export const orderApiSlice = apiSlice.injectEndpoints({
                 url: PAYPAL_URL,
             }),
             keepUnusedDataFor: 5,
+        }),
+        getMyOrders: builder.query({
+            query: ()=>({
+                url:`${ORDERS_URL}/mine`,
+            }),
+            keepUnusedDataFor: 5, 
         })
     }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery } =
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery, useGetMyOrdersQuery } =
     orderApiSlice;
