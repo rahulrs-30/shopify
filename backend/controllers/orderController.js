dcf6ecac52c3f3343b2a41267e58216706408895
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-27 13:21:40 +0530

    order delivered with some issue

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,8 +4,8 @@ import Order from "../models/orderModel.js";
 //@desc Create new Order
 //@route POST /api/orders
 // @access Private
-const addOrderItems = asyncHandler(async (req,res) => {
-    const { 
+const addOrderItems = asyncHandler(async (req, res) => {
+    const {
         orderItems,
         shippingAddress,
         paymentMethod,
@@ -15,12 +15,12 @@ const addOrderItems = asyncHandler(async (req,res) => {
         totalPrice,
     } = req.body;
 
-    if(orderItems && orderItems.length === 0){
+    if (orderItems && orderItems.length === 0) {
         res.status(400);
         throw new Error("No order Items");
-    }else{
-        const order =new Order({
-            orderItems: orderItems.map((x)=>({
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
                 ...x,
                 product: x._id,
                 _id: undefined
@@ -41,20 +41,20 @@ const addOrderItems = asyncHandler(async (req,res) => {
 //@desc Get logged in users orders
 //@route GET /api/orders/myorders
 // @access Private
-const getMyOrders = asyncHandler(async (req,res) => {
-    const orders = await Order.find({user: req.user._id});
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
     res.status(200).json(orders);
 });
 
 //@desc Get orders by ID
 //@route GET /api/orders/:id
 // @access Private
-const getOrderById = asyncHandler(async (req,res) => {
-    const order = await Order.findById(req.params.id).populate('user','name email');
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
 
-    if(order){
+    if (order) {
         res.status(200).json(order);
-    }else{
+    } else {
         res.status(404).json('No such order found');
     }
 });
@@ -62,38 +62,44 @@ const getOrderById = asyncHandler(async (req,res) => {
 //@desc Update order to paid
 //@route PUT /api/orders/:id/pay
 // @access Private
-const updateOrderToPaid = asyncHandler(async (req,res) => {
+const updateOrderToPaid = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
-    if(order){
-        order.isPaid = true;
-        order.paidAt = Date.now();
-        order.paymentResult = {
-            id: req.body.id,
-            status: req.body.status,
-            update_time: req.body.update_time,
-            email_address: req.body.payer.email_address
-        }
-        const updatedOrder = await order.save();
-        res.status(200).json(updatedOrder);
-    }else{
-        res.status(404);
-        throw new Error("Order not Found");
-    }
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 //@desc Update order to delivered
 //@route PUT /api/orders/:id/deliver
 // @access Private/Admin
-const updateOrderToDelivered = asyncHandler(async (req,res) => {
-    res.send('update order to delivered');
-}); 
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+        const updatedOrder = await order.save();
+        res.status(200).json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error("Order Not found.");
+    }
+});
 
 //@desc Get all orders
 //@route GET /api/orders
 // @access Private/Admin
-const getOrders = asyncHandler(async (req,res) => {
+const getOrders = asyncHandler(async (req, res) => {
     const orders = await Order.find({}).populate('user', 'id name');
     res.status(200).json(orders);
 });
 
-export {addOrderItems, getMyOrders, getOrderById, updateOrderToPaid, updateOrderToDelivered, getOrders};
+export { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid, updateOrderToDelivered, getOrders };
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -8,9 +8,10 @@ import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import {
+  useDeliverOrderMutation,
   useGetOrderDetailsQuery,
-  usePayOrderMutation,
   useGetPayPalClientIdQuery,
+  usePayOrderMutation,
 } from "../slices/ordersApiSlice";
 
 const OrderScreen = () => {
@@ -24,6 +25,7 @@ const OrderScreen = () => {
   } = useGetOrderDetailsQuery(orderId);
 
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+  const [deliverOrder, { isLoading: loadingDeliver }] = useDeliverOrderMutation();
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
   const {
     data: paypal,
@@ -88,6 +90,16 @@ const OrderScreen = () => {
       .then((orderId) => {
         return orderId;
       });
+  }
+
+  const deliverOrderHandler = async () => {
+    try {
+      await deliverOrder(orderId);
+      refetch();
+      toast.success('Order Delivered');
+    } catch (err) {
+      toast.error(err?.data?.message || err.message);
+    }
   }
 
   return isLoading ? (
@@ -203,7 +215,22 @@ const OrderScreen = () => {
                   )}
                 </ListGroup.Item>
               )}
-              {/* MARK AS DELIVERED PLACEHOLDER */}
+              {loadingDeliver && <Loader />}
+
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type='button'
+                      className='btn btn-block'
+                      onClick={deliverOrderHandler}
+                    >
+                      Mark As Delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
             </ListGroup>
           </Card>
         </Col>
