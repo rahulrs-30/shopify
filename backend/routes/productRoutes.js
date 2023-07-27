4652a9bdb976b8709bb36f218745c68fc58cfe17
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-27 14:10:44 +0530

    create new product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router =express.Router();
-import {getProducts , getProductById} from '../controllers/productController.js';
+import {getProducts , getProductById, createProduct} from '../controllers/productController.js';
+import {protect, admin} from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts); //GET all products
+router.route('/').get(getProducts).post(protect, admin, createProduct); //GET all products
 
 router.route('/:id').get(getProductById) ;// GET product by id  /:id
 
