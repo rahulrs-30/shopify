618b7e6ee866f71b4d1f995acf504e07e7526dae
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-27 19:09:33 +0530

    edit product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router =express.Router();
-import {getProducts , getProductById, createProduct} from '../controllers/productController.js';
+import {getProducts , getProductById, createProduct, updateProduct} from '../controllers/productController.js';
 import {protect, admin} from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct); //GET all products
 
-router.route('/:id').get(getProductById) ;// GET product by id  /:id
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct) ;// GET product by id  /:id
+
 
 export default router;
