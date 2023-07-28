84ac557431346b86496090c98174e3b56a0a5114
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-29 01:56:43 +0530

    preparation for production

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+
 //Product routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -37,6 +35,23 @@ app.get("/api/config/paypal", (req, res) =>
 
 const  __dirname =path.resolve(); //set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
+
+
+
+
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    //any route that is not api will be directed to index.html
+    app.get('*',(req,res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'))
+    );
+}else{
+    app.get("/", (req, res) => {
+        res.send("API is running...");
+    });
+}
 
 //Error handling middleware 404 and other errors
 app.use(notFound);
